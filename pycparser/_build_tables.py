39219c141c23c16728f201d192a8bf1e77d2eab3
Eli Bendersky <eliben@gmail.com>
2013-06-12 06:18:37 -0700

    Updated CHANGES and some cosmetic cleanups

--- pycparser/_build_tables.py
+++ pycparser/_build_tables.py
@@ -1,7 +1,7 @@
 #-----------------------------------------------------------------
 # pycparser: _build_tables.py
 #
-# A dummy for generating the lexing/parsing tables and and 
+# A dummy for generating the lexing/parsing tables and and
 # compiling them into .pyc for faster execution in optimized mode.
 # Also generates AST code from the configuration file.
 # Should be called from the pycparser directory.
@@ -23,8 +23,8 @@ from pycparser import c_parser
 # Generates the tables
 #
 c_parser.CParser(
-    lex_optimize=True, 
-    yacc_debug=False, 
+    lex_optimize=True,
+    yacc_debug=False,
     yacc_optimize=True)
 
 # Load to compile into .pyc
