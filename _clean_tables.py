e936d41acf13ae8995c3cdef01ca641917ef2db4
Eli Bendersky <eliben@google.com>
2015-04-20 15:53:11 -0700

    Clean-up _clean_tables and make it Python3-friendly

--- _clean_tables.py
+++ _clean_tables.py
@@ -1,9 +1,10 @@
-# Cleanup all table and PYC files to ensure no PLY stuff is cached
-#
+# Cleanup all tables and PYC files to ensure no PLY stuff is cached
+from __future__ import print_function
 import fnmatch
 import os, shutil
 
-file_patterns = ('yacctab.*', 'lextab.*', '*.pyc')
+file_patterns = ('yacctab.*', 'lextab.*', '*.pyc', '__pycache__')
+
 
 def do_cleanup(root):
     for path, dirs, files in os.walk(root):
@@ -13,12 +14,10 @@ def do_cleanup(root):
                     if fnmatch.fnmatch(file, pattern):
                         fullpath = os.path.join(path, file)
                         os.remove(fullpath)
-                        print 'Deleted', fullpath
+                        print('Deleted', fullpath)
             except OSError:
                 pass
 
+
 if __name__ == "__main__":
     do_cleanup('.')
-
-
-    
