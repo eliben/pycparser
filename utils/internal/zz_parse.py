43375bf83890a77b31f3971329de25fc0764a276
Eli Bendersky <eliben@gmail.com>
2015-04-20 07:19:09 -0700

    Clean up line endings in all internal utils

--- utils/internal/zz_parse.py
+++ utils/internal/zz_parse.py
@@ -8,10 +8,14 @@ if __name__ == "__main__":
     #ast = parse_file('zc_pp.c', use_cpp=True, cpp_path="../cpp.exe")
     parser = c_parser.CParser()
 
-    code = r'''int ar[30];'''
+    #code = r'''int ar[30];'''
+    code = r'''
+    char ***arr3d[40];
+    '''
 
     #code = r'''
     #int foo(int a, int arr[*]);
             #'''
+    print(code)
     ast = parser.parse(code)
     ast.show(attrnames=True, nodenames=True)
