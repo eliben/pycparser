30c38a862647cadb73a3c20bb3f33cc398f399f0
Michael White <mikewhite22@yahoo.com>
2017-01-30 02:49:02 -0800

    Now roundtrippable

--- examples/c_json.py
+++ examples/c_json.py
@@ -9,21 +9,23 @@ import re
 #
 sys.path.extend(['.', '..'])
 
-from pycparser import parse_file, c_ast, c_parser, c_generator
+from pycparser import parse_file, c_ast
+
 
 RE_CHILD_ARRAY = re.compile('(.*)\[(.*)\]')
+
 
 class CJsonError(Exception):
     pass
 
-def to_dict(node):
-    """ Recursively convert an ast into a python dictionary. """
 
+def to_dict(node):
+    """ Recursively convert an ast into a dict representation """
     klass = node.__class__
     attr_names = klass.attr_names
 
     result = {}
-    result['node_class'] = klass.__name__
+    result['_nodetype'] = klass.__name__
     for attr in klass.attr_names:
         result[attr] = getattr(node, attr)
 
@@ -47,18 +49,76 @@ def to_dict(node):
 
 
 def to_json(node, **kwargs):
+    """ Convert ast node to json string """
     return json.dumps(to_dict(node), **kwargs)
 
 
+def file_to_dict(filename):
+    """ Load C file into dict representation of ast """
+    ast = parse_file(filename, use_cpp=True)
+    return to_dict(ast)
+
+
 def file_to_json(filename, **kwargs):
+    """ Load C file into json string representation of ast """
     ast = parse_file(filename, use_cpp=True)
     return to_json(ast, **kwargs)
+
+
+def _convert_to_obj(value):
+    """
+    Convert an object in the dict representation into an object.
+    Note: Mutually recursive with from_dict.
+
+    """
+    value_type = type(value)
+    if value_type == dict:
+        return from_dict(value)
+    elif value_type == list:
+        return [_convert_to_obj(item) for item in value]
+    else:
+        # String
+        return value
+
+
+def from_dict(node_dict):
+    """ Recursively build an ast from dict representation """
+    class_name = node_dict.pop('_nodetype')
+
+    klass = getattr(c_ast, class_name)
+
+    # Create a new dict containing the key-value pairs which we can pass
+    # to node constructors.
+    objs = {}
+    for key, value in node_dict.items():
+        objs[key] = _convert_to_obj(value)
+
+    # Need to set values for empty child nodes. This might be better accomplished with "=None"
+    # default values in the constructors, but I'm trying implement this without touching the
+    # main codebase.
+    slots = set(klass.__slots__)
+    slots.discard('__weakref__')
+    for slot in slots:
+        if slot not in objs:
+            objs[slot] = None
+
+    # Use keyword parameters, which works thanks to beautifully consistent
+    # ast Node initializers.
+    return klass(**objs)
+
+
+def from_json(ast_json):
+    """ Build an ast from json string representation """
+    return from_dict(json.loads(ast_json))
 
 
 #------------------------------------------------------------------------------
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-       print(file_to_json(sys.argv[1], sort_keys=True, indent=4))
-
+        # Some test code...
+        # Do trip from C -> ast -> dict -> ast -> json, then print.
+        ast_dict = file_to_dict(sys.argv[1])
+        ast = from_dict(ast_dict)
+        print(to_json(ast, sort_keys=True, indent=4))
     else:
         print("Please provide a filename as argument")
