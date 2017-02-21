498bb0768484360f4c6ae0c51c48b9b2110c680a
Nate Bogdanowicz <natezb@gmail.com>
2017-02-20 16:01:18 -0800

    Add more documentation of the templating functions

--- pycparser/plyparser.py
+++ pycparser/plyparser.py
@@ -72,6 +72,10 @@ def parameterized(*params):
 
 
 def template(cls):
+    """ Class decorator to generate rules from parameterized rule templates.
+
+    See `parameterized` for more information on parameterized rules.
+    """
     for attr_name in dir(cls):
         if attr_name.startswith('p_'):
             method = getattr(cls, attr_name)
@@ -82,11 +86,20 @@ def template(cls):
 
 
 def _create_param_rules(cls, func):
-    """ Creates ply.yacc rules based on a parameterized rule function """
+    """ Create ply.yacc rules based on a parameterized rule function
+
+    Generates new methods (one per each pair of parameters) based on the template
+    rule function `func`, and attaches them to `cls`. The rule function's
+    parameters must be accessible via its `_params` attribute.
+    """
     for xxx, yyy in func._params:
+        # Use the template method's body for each new method
         def param_rule(self, p):
             func(self, p)
 
+        # Substitute in the params for the grammar rule and function name
         param_rule.__doc__ = func.__doc__.replace('xxx', xxx).replace('yyy', yyy)
         param_rule.__name__ = func.__name__.replace('xxx', xxx)
+
+        # Attach the new method to the class
         setattr(cls, param_rule.__name__, param_rule)
