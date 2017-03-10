471442fe06b095c085d0219a95fbf1f177b6449b
serpilliere <serpilliere@users.noreply.github.com>
2017-03-10 15:02:00 +0100

    Add column support in c_parser (#178)

--- pycparser/plyparser.py
+++ pycparser/plyparser.py
@@ -51,6 +51,17 @@ class PLYParser(object):
                 line=lineno,
                 column=column)
 
+    def _token_coord(self, p, token_idx):
+        """ Returns the coordinates for the YaccProduction objet 'p' indexed
+            with 'token_idx'. The coordinate includes the 'lineno' and
+            'column'. Both follow the lex semantic, starting from 1.
+        """
+        last_cr = p.lexer.lexer.lexdata.rfind('\n', 0, p.lexpos(token_idx))
+        if last_cr < 0:
+            last_cr = -1
+        column = (p.lexpos(token_idx) - (last_cr))
+        return self._coord(p.lineno(token_idx), column)
+
     def _parse_error(self, msg, coord):
         raise ParseError("%s: %s" % (coord, msg))
 
