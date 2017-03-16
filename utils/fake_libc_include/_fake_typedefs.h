c7b929cab09fc4332e13f3ecaa626e2afb082a9d
Jean-Sébastien B <realitix@gmail.com>
2017-03-16 14:47:54 +0100

    Add fakedef (#183)
    * Add Mir typedefs in fake headers
    
    * Add xcb includes to fake includes

--- utils/fake_libc_include/_fake_typedefs.h
+++ utils/fake_libc_include/_fake_typedefs.h
@@ -158,4 +158,22 @@ typedef unsigned long XID;
 typedef unsigned long VisualID;
 typedef XID Window;
 
+/* Mir typedefs */
+typedef void* MirEGLNativeWindowType;
+typedef void* MirEGLNativeDisplayType;
+typedef struct MirConnection MirConnection;
+typedef struct MirSurface MirSurface;
+typedef struct MirSurfaceSpec MirSurfaceSpec;
+typedef struct MirScreencast MirScreencast;
+typedef struct MirPromptSession MirPromptSession;
+typedef struct MirBufferStream MirBufferStream;
+typedef struct MirPersistentId MirPersistentId;
+typedef struct MirBlob MirBlob;
+typedef struct MirDisplayConfig MirDisplayConfig;
+
+/* xcb typedefs */
+typedef struct xcb_connection_t xcb_connection_t;
+typedef uint32_t xcb_window_t;
+typedef uint32_t xcb_visualid_t;
+
 #endif
