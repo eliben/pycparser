0411963d552207991294347dc75d606b60685334
Eli Bendersky <eliben@gmail.com>
2018-09-30 05:30:03 -0700

    Add option to dump_ast example to show coordinates

--- examples/dump_ast.py
+++ examples/dump_ast.py
@@ -19,7 +19,9 @@ from pycparser import c_parser, c_ast, parse_file
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser('Dump AST')
     argparser.add_argument('filename', help='name of file to parse')
+    argparser.add_argument('--coord', help='show coordinates in the dump',
+                           action='store_true')
     args = argparser.parse_args()
 
     ast = parse_file(args.filename, use_cpp=False)
-    ast.show()
+    ast.show(showcoord=args.coord)
