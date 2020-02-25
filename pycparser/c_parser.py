0b406fd8847c30fe7a1c277ff16e118f258c16d1
Zecong Hu <huzecong@gmail.com>
2020-02-26 00:15:42 +0800

    Fix #363 incorrect AST when parsing offsetof

--- pycparser/c_parser.py
+++ pycparser/c_parser.py
@@ -1740,8 +1740,7 @@ class CParser(PLYParser):
         if len(p) == 2:
             p[0] = p[1]
         elif len(p) == 4:
-            field = c_ast.ID(p[3], self._token_coord(p, 3))
-            p[0] = c_ast.StructRef(p[1], p[2], field, p[1].coord)
+            p[0] = c_ast.StructRef(p[1], p[2], p[3], p[1].coord)
         elif len(p) == 5:
             p[0] = c_ast.ArrayRef(p[1], p[3], p[1].coord)
         else:
