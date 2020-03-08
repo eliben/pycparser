6ae2be123b3d74d10b819632b131fd282145db5d
Thomas Nilefalk <thomas@nilefalk.se>
2020-03-08 08:57:19 +0100

    Add some "types" for zlib

--- utils/fake_libc_include/zlib.h
+++ utils/fake_libc_include/zlib.h
@@ -1,2 +1,21 @@
 #include "_fake_defines.h"
 #include "_fake_typedefs.h"
+
+#define uInt int
+#define uLong int
+#define Byte int
+
+/* Some FAR types */
+#define Bytef int
+#define charf int
+#define inft inf
+
+#define voidpc int
+#define voidpf int
+#define voidp int
+
+#define z_crc_t int
+#define z_size_t int
+
+#define alloc_func int
+#define free_func int
