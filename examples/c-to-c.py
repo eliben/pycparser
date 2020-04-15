7b893b86bb185ff1262f6a524e92b8ad592acaa3
Eli Bendersky <eliben@gmail.com>
2020-04-15 15:04:29 -0700

    Move internal testing stuff from c-to-c example into utils/internal

--- examples/c-to-c.py
+++ examples/c-to-c.py
@@ -12,10 +12,9 @@ import sys
 
 # This is not required if you've installed pycparser into
 # your site-packages/ with setup.py
-#
 sys.path.extend(['.', '..'])
 
-from pycparser import parse_file, c_parser, c_generator
+from pycparser import parse_file, c_generator
 
 
 def translate_to_c(filename):
@@ -26,36 +25,7 @@ def translate_to_c(filename):
     print(generator.visit(ast))
 
 
-def _zz_test_translate():
-    # internal use
-    src = r'''
-
-    void f(char * restrict joe){}
-
-int main(void)
-{
-    unsigned int long k = 4;
-    int p = - - k;
-    return 0;
-}
-'''
-    parser = c_parser.CParser()
-    ast = parser.parse(src)
-    ast.show()
-    generator = c_generator.CGenerator()
-
-    print(generator.visit(ast))
-
-    # tracing the generator for debugging
-    #~ import trace
-    #~ tr = trace.Trace(countcallers=1)
-    #~ tr.runfunc(generator.visit, ast)
-    #~ tr.results().write_results()
-
-
-#------------------------------------------------------------------------------
 if __name__ == "__main__":
-    #_zz_test_translate()
     if len(sys.argv) > 1:
         translate_to_c(sys.argv[1])
     else:
