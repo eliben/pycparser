d31fe8bd9ac69e0f80428ae9558563ee63cb142b
Burkhard Ringlein <ngl@zurich.ibm.com>
2021-03-12 11:11:10 +0100

    fixing limits in fake include

--- utils/fake_libc_include/_fake_defines.h
+++ utils/fake_libc_include/_fake_defines.h
@@ -1,19 +1,19 @@
 #ifndef _FAKE_DEFINES_H
 #define _FAKE_DEFINES_H
 
-#define	NULL	0
-#define	BUFSIZ		1024
-#define	FOPEN_MAX	20
-#define	FILENAME_MAX	1024
+#define NULL  0
+#define BUFSIZ    1024
+#define FOPEN_MAX 20
+#define FILENAME_MAX  1024
 
 #ifndef SEEK_SET
-#define	SEEK_SET	0	/* set file offset to offset */
+#define SEEK_SET  0 /* set file offset to offset */
 #endif
 #ifndef SEEK_CUR
-#define	SEEK_CUR	1	/* set file offset to current plus offset */
+#define SEEK_CUR  1 /* set file offset to current plus offset */
 #endif
 #ifndef SEEK_END
-#define	SEEK_END	2	/* set file offset to EOF plus offset */
+#define SEEK_END  2 /* set file offset to EOF plus offset */
 #endif
 
 #define __LITTLE_ENDIAN 1234
@@ -26,11 +26,19 @@
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
 
+#define CHAR_MIN -128
+#define CHAR_MAX 127
 #define UCHAR_MAX 255
+#define SHRT_MIN -32768
+#define SHRT_MAX 32767
 #define USHRT_MAX 65535
+#define INT_MAX 2147483647
+#define INT_MIN -2147483648
 #define UINT_MAX 4294967295U
+#define LONG_MAX 9223372036854775807L
+#define LONG_MIN -9223372036854775808L
+#define ULONG_MAX 18446744073709551615UL
 #define RAND_MAX 32767
-#define INT_MAX 32767
 
 /* C99 inttypes.h defines */
 #define PRId8 "d"
