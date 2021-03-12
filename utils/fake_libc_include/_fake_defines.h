e1e940d6ebcafa2ad4085bcae28bc4104fe03ec2
Burkhard Ringlein <ngl@zurich.ibm.com>
2021-03-12 11:14:03 +0100

    redo retab

--- utils/fake_libc_include/_fake_defines.h
+++ utils/fake_libc_include/_fake_defines.h
@@ -1,19 +1,19 @@
 #ifndef _FAKE_DEFINES_H
 #define _FAKE_DEFINES_H
 
-#define NULL  0
-#define BUFSIZ    1024
-#define FOPEN_MAX 20
-#define FILENAME_MAX  1024
+#define	NULL	0
+#define	BUFSIZ		1024
+#define	FOPEN_MAX	20
+#define	FILENAME_MAX	1024
 
 #ifndef SEEK_SET
-#define SEEK_SET  0 /* set file offset to offset */
+#define	SEEK_SET	0	/* set file offset to offset */
 #endif
 #ifndef SEEK_CUR
-#define SEEK_CUR  1 /* set file offset to current plus offset */
+#define	SEEK_CUR	1	/* set file offset to current plus offset */
 #endif
 #ifndef SEEK_END
-#define SEEK_END  2 /* set file offset to EOF plus offset */
+#define	SEEK_END	2	/* set file offset to EOF plus offset */
 #endif
 
 #define __LITTLE_ENDIAN 1234
