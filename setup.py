3d3b715f22e0f98dbb90686fad5be25d14ae75e8
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-09-23 21:41:44 +0300

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     packages=['pycparser', 'pycparser.ply'],
