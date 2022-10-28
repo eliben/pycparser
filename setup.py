b13a06d22d05d47f115f3ed141f4fa727c654c6d
Erlend E. Aasland <erlend.aasland@protonmail.com>
2022-10-28 14:41:20 +0200

    Add Python 3.11 as a supported version (#469)
    * Add Python 3.11 as a supported version
    
    Fixes #468
    
    * Address review: add Python 3.11 to CI test matrix

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     packages=['pycparser', 'pycparser.ply'],
