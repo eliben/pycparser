52622576e92827baed22c96fdb52c37da1094b22
brent hartshorn <brenthartshorn@proton.me>
2024-08-16 20:08:36 -0700

    holyC to C

--- holypycparser.py
+++ holypycparser.py
@@ -3,21 +3,27 @@ import os, sys
 assert os.path.isdir('./pycparser')
 sys.path.insert(0, './pycparser')
 import pycparser
-print(pycparser)
+from pycparser import c_generator
 
 def parse(hc):
 	print('parsing:', hc)
 	ast = pycparser.parse_file( hc )
 	print(ast)
+	return ast
 
 HW = r'''
 U0 Hello(){
     "Hello World\n";
-	Hello;
 }
 Hello;
 Hello();
+I32 add(I32 x="foooo", I32 y){
+    return x+y;
+}
+I32 X;
+X = add(8);
 '''
+
 
 if __name__=='__main__':
 	test = None
@@ -29,4 +35,8 @@ if __name__=='__main__':
 		test = '/tmp/hw.HC'
 		open(test,'wb').write(HW.encode('utf-8'))
 
-	parse(test)
+	ast = parse(test)
+	gen = c_generator.CGenerator()
+	print(gen)
+	c = gen.visit(ast)
+	print(c)
